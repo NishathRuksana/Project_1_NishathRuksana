dd6f4a5530fc7e842c3a46bc9f88443be8ec1570
Nishath Ruksana <nishathruksana@gmail.com>
2025-02-14 16:48:01 -0700

    Initial Commit

---
+++ application.py
@@ -0,0 +1,36 @@
+import os
+
+from flask import Flask, session
+from flask_session import Session
+from sqlalchemy import create_engine,text
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+@app.route("/")
+def index():
+    return "Project 1: TODO"
+
+@app.route("/test-db")
+def test_db():
+    try:
+        # Attempt to execute a simple query
+        db.execute(text('SELECT 1'))
+        return "Database connection successful!"
+    except Exception as e:
+        return f"Database connection failed: {str(e)}"
+
